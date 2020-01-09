efc9be7db4ffa3c09761e8841ec96c063f38aec5
Amit Kumar <amitnarayansah@gmail.com>
2020-01-10 00:08:21 +0530

    Login & Register  added UserName and changed for email login

--- AprajitaRetails/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ AprajitaRetails/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -43,7 +43,7 @@ namespace AprajitaRetails.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
-            [EmailAddress]
+            [Display(Name = "User Name")]
             public string Email { get; set; }
 
             [Required]
