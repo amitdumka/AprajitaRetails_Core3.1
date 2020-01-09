51f643533227cdba925c3d7ab48c188de8353988
Amit Kumar <amitnarayansah@gmail.com>
2020-01-09 10:22:21 +0530

    Add project files.

---
+++ AprajitaRetails/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AprajitaRetails.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AprajitaRetails.Areas.Identity.IdentityHostingStartup))]
+namespace AprajitaRetails.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
