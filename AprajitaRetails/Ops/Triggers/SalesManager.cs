2644fbc588acc7e4e05de91cd088c8d1fd7e70b5
Amit Kumar <amitnarayansah@gmail.com>
2020-01-13 14:10:21 +0530

    edit trigger for dailysale, still need to add more

--- AprajitaRetails/Ops/Triggers/SalesManager.cs
+++ AprajitaRetails/Ops/Triggers/SalesManager.cs
@@ -9,82 +9,83 @@ namespace AprajitaRetails.Ops.Triggers
 {
     public class SalesManager
     {
-        private void UpDateAmount(AprajitaRetailsContext db,PayModes PayMode, DateTime SaleDate, decimal CashAmount)
+        private void UpDateAmount(AprajitaRetailsContext db, DailySale dailySale, bool IsEdit)
         {
-            if (PayMode == PayModes.Cash && CashAmount > 0)
+            if (IsEdit)
             {
-                CashTrigger.UpdateCashInHand(db, SaleDate, CashAmount);
-
-            }
-            //TODO: in future make it more robust
-            if (PayMode != PayModes.Cash && PayMode != PayModes.Coupons && PayMode != PayModes.Points)
-            {
-                CashTrigger.UpdateCashInBank(db, SaleDate, CashAmount);
-            }
-        }
-        private void UpdateDueAmount(AprajitaRetailsContext db, PayModes PayMode, DateTime SaleDate, Decimal CashAmount)
-        {
-
-        }
-
-        private void UpdateSalesRetun(AprajitaRetailsContext db, DailySale dailySale) {
-
-            if (dailySale.PayMode == PayModes.Cash && dailySale.CashAmount > 0)
-            {
-                CashTrigger.UpDateCashOutHand(db, dailySale.SaleDate, dailySale.CashAmount);
-
-            }
-            //TODO: in future make it more robust
-            if (dailySale.PayMode != PayModes.Cash && dailySale.PayMode != PayModes.Coupons && dailySale.PayMode != PayModes.Points)
-            {
-                CashTrigger.UpDateCashOutBank(db, dailySale.SaleDate, dailySale.Amount - dailySale.CashAmount);
-            }
-            dailySale.Amount = 0 - dailySale.Amount;
-
-        }
-
-
-        public void ProcessAccounts(AprajitaRetailsContext db, DailySale dailySale)
-        {
-            if (!dailySale.IsSaleReturn)
-            {
-                if (!dailySale.IsDue)
+                if (dailySale.PayMode == PayModes.Cash && dailySale.CashAmount > 0)
                 {
-                    if (dailySale.PayMode == PayModes.Cash && dailySale.CashAmount > 0)
-                    {
-                        CashTrigger.UpdateCashInHand(db, dailySale.SaleDate, dailySale.CashAmount);
+                    CashTrigger.UpdateCashInHand(db, dailySale.SaleDate, 0 - dailySale.CashAmount);
 
-                    }
-                    //TODO: in future make it more robust
-                    if (dailySale.PayMode != PayModes.Cash && dailySale.PayMode != PayModes.Coupons && dailySale.PayMode != PayModes.Points)
-                    {
-                        CashTrigger.UpdateCashInBank(db, dailySale.SaleDate, dailySale.Amount - dailySale.CashAmount);
-                    }
+                }
+                //TODO: in future make it more robust
+                if (dailySale.PayMode != PayModes.Cash && dailySale.PayMode != PayModes.Coupons && dailySale.PayMode != PayModes.Points)
+                {
+                    CashTrigger.UpdateCashInBank(db, dailySale.SaleDate, 0 - (dailySale.Amount - dailySale.CashAmount));
+                }
+            }
+            else
+            {
+                if (dailySale.PayMode == PayModes.Cash && dailySale.CashAmount > 0)
+                {
+                    CashTrigger.UpdateCashInHand(db, dailySale.SaleDate, dailySale.CashAmount);
+
+                }
+                //TODO: in future make it more robust
+                if (dailySale.PayMode != PayModes.Cash && dailySale.PayMode != PayModes.Coupons && dailySale.PayMode != PayModes.Points)
+                {
+                    CashTrigger.UpdateCashInBank(db, dailySale.SaleDate, dailySale.Amount - dailySale.CashAmount);
+                }
+            }
+
+        }
+        private void UpdateDueAmount(AprajitaRetailsContext db, DailySale dailySale, bool IsEdit)
+        {
+            if (IsEdit)
+            {
+                var dId = db.DuesLists.Where(c => c.DailySaleId == dailySale.DailySaleId).FirstOrDefault();
+                if (dId != null)
+                {
+                    db.DuesLists.Remove(dId);
                 }
                 else
                 {
-                    decimal dueAmt;
-                    if (dailySale.Amount != dailySale.CashAmount)
-                    {
-                        dueAmt = dailySale.Amount - dailySale.CashAmount;
-                    }
-                    else
-                        dueAmt = dailySale.Amount;
-
-                    DuesList dl = new DuesList() { Amount = dueAmt, DailySale = dailySale, DailySaleId = dailySale.DailySaleId };
-                    db.DuesLists.Add(dl);
-
-                    if (dailySale.PayMode == PayModes.Cash && dailySale.CashAmount > 0)
-                    {
-                        CashTrigger.UpdateCashInHand(db, dailySale.SaleDate, dailySale.CashAmount);
-
-                    }
-                    //TODO: in future make it more robust
-                    if (dailySale.PayMode != PayModes.Cash && dailySale.PayMode != PayModes.Coupons && dailySale.PayMode != PayModes.Points)
-                    {
-                        CashTrigger.UpdateCashInBank(db, dailySale.SaleDate, dailySale.Amount - dailySale.CashAmount);
-                    }
+                    //TODO: Handle this
                 }
+            }
+            else
+            {
+                decimal dueAmt;
+                if (dailySale.Amount != dailySale.CashAmount)
+                {
+                    dueAmt = dailySale.Amount - dailySale.CashAmount;
+                }
+                else
+                    dueAmt = dailySale.Amount;
+
+                DuesList dl = new DuesList() { Amount = dueAmt, DailySale = dailySale, DailySaleId = dailySale.DailySaleId };
+                db.DuesLists.Add(dl);
+            }
+
+
+        }
+
+        private void UpdateSalesRetun(AprajitaRetailsContext db, DailySale dailySale, bool IsEdit)
+        {
+
+            if (IsEdit)
+            {
+                if (dailySale.PayMode == PayModes.Cash && dailySale.CashAmount > 0)
+                {
+                    CashTrigger.UpDateCashOutHand(db, dailySale.SaleDate, 0 - dailySale.CashAmount);
+
+                }
+                //TODO: in future make it more robust
+                if (dailySale.PayMode != PayModes.Cash && dailySale.PayMode != PayModes.Coupons && dailySale.PayMode != PayModes.Points)
+                {
+                    CashTrigger.UpDateCashOutBank(db, dailySale.SaleDate, 0 - (dailySale.Amount - dailySale.CashAmount));
+                }
+                //dailySale.Amount = 0 - dailySale.Amount;
             }
             else
             {
@@ -99,6 +100,29 @@ namespace AprajitaRetails.Ops.Triggers
                     CashTrigger.UpDateCashOutBank(db, dailySale.SaleDate, dailySale.Amount - dailySale.CashAmount);
                 }
                 dailySale.Amount = 0 - dailySale.Amount;
+            }
+
+
+        }
+
+
+        public void ProcessAccounts(AprajitaRetailsContext db, DailySale dailySale)
+        {
+            if (!dailySale.IsSaleReturn)
+            {
+                if (!dailySale.IsDue)
+                {
+                    UpDateAmount(db, dailySale, false);
+                }
+                else
+                {
+                    UpdateDueAmount(db, dailySale, false);
+                    UpDateAmount(db, dailySale, false);
+                }
+            }
+            else
+            {
+                UpdateSalesRetun(db, dailySale, false);
 
             }
 
@@ -106,70 +130,73 @@ namespace AprajitaRetails.Ops.Triggers
 
         public void ProcessAccountDelete(AprajitaRetailsContext db, DailySale dailySale)
         {
-            if (dailySale.PayMode == PayModes.Cash && dailySale.CashAmount > 0)
+            //TODO: Handle for Dues 
+            if (dailySale.IsSaleReturn)
             {
-                CashTrigger.UpdateCashInHand(db, dailySale.SaleDate, 0 - dailySale.CashAmount);
-            }
-            else if (dailySale.PayMode != PayModes.Cash && dailySale.PayMode != PayModes.Coupons && dailySale.PayMode != PayModes.Points)
-            {
-                CashTrigger.UpdateCashInBank(db, dailySale.SaleDate, 0 - dailySale.CashAmount);
+                UpdateSalesRetun(db, dailySale, true);
             }
             else
             {
-                //TODO: Add this option in Create and Edit also
-                // Handle when payment is done by Coupons and Points. 
-                // Need to create table to create Coupn and Royalty point.
-                // Points will go in head for Direct Expenses 
-                // Coupon Table will be colloum for TAS Coupon and Apajita Retails. 
-                
-                //TODO: Need to handle is. 
-                // If payment is cash and cashamount is zero then need to handle this option also 
-                // may be error entry , might be due.
-                
-                throw new Exception();
+                if (dailySale.IsDue)
+                {
+                    UpdateSalesRetun(db, dailySale, true);
+                }
+                else
+                {
+                    //TODO: Add this option in Create and Edit also
+                    // Handle when payment is done by Coupons and Points. 
+                    // Need to create table to create Coupn and Royalty point.
+                    // Points will go in head for Direct Expenses 
+                    // Coupon Table will be colloum for TAS Coupon and Apajita Retails. 
+
+                    //TODO: Need to handle is. 
+                    // If payment is cash and cashamount is zero then need to handle this option also 
+                    // may be error entry , might be due.
+
+                    throw new Exception();
+                }
+
+                UpDateAmount(db, dailySale, true);
             }
+
+
         }
 
         public void ProcessAccountEdit(AprajitaRetailsContext db, DailySale dailySale)
         {
             var oldSale = db.DailySales.Find(dailySale.DailySaleId);
 
-            if (oldSale.PayMode == PayModes.Cash) 
-            {
-                CashTrigger.UpDateCashOutHand(db, oldSale.SaleDate, oldSale.CashAmount);
-                
+            UpDateAmount(db, oldSale, true);
 
+            if (oldSale.IsSaleReturn)
+            {
+                // SaleRetun
             }
             else
             {
-                CashTrigger.UpDateCashOutBank(db, oldSale.SaleDate, oldSale.CashAmount);
-
-            }
-            if (oldSale.IsDue)
-            {
-                if (!oldSale.IsDue)
+                // Normal Bill
+                if (oldSale.IsDue)
                 {
-                    //TODO: Remove due
+                    if (!dailySale.IsDue)
+                    {
+                        UpdateSalesRetun(db, oldSale, true);
+                    }
+                    else
+                    {
+
+                    }
                 }
+                else
+                { //TODO: Add due
+                }
+
+                UpDateAmount(db, oldSale, true);
+                UpDateAmount(db, dailySale, false);
             }
-            else { //TODO: Add due
-            }
+
+
+
             
-            if (oldSale.IsSaleReturn) { } else { }
-            
-
-
-
-            if (dailySale.PayMode == PayModes.Cash && dailySale.CashAmount > 0)
-            {
-                CashTrigger.UpdateCashInHand(db, dailySale.SaleDate, dailySale.CashAmount);
-
-            }
-            //TODO: in future make it more robust
-            if (dailySale.PayMode != PayModes.Cash && dailySale.PayMode != PayModes.Coupons && dailySale.PayMode != PayModes.Points)
-            {
-                CashTrigger.UpdateCashInBank(db, dailySale.SaleDate, dailySale.Amount - dailySale.CashAmount);
-            }
         }
     }
 }
