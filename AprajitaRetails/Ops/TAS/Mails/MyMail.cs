a0c112d45f0829b2aceb6acbfe4111ccad146ebb
Amit Kumar <amitnarayansah@gmail.com>
2020-02-03 01:22:42 +0530

    MLogs (Mail Logs added)

--- AprajitaRetails/Ops/TAS/Mails/MyMail.cs
+++ AprajitaRetails/Ops/TAS/Mails/MyMail.cs
@@ -7,7 +7,6 @@ namespace AprajitaRetails.Ops.TAS.Mails
 
     public static class MyMail
     {
-
         public static void SendEmail(string subjects, string messages, string toAddress)
         {
             var message = new MimeMessage ();
@@ -27,6 +26,41 @@ namespace AprajitaRetails.Ops.TAS.Mails
             client.Send (message);
             client.Disconnect (true);
         }
+    }
+
+    public static class MLogs
+    {
+        public static void MailLog(string messages, string? subjects)
+        {
+
+            var message = new MimeMessage ();
+            message.From.Add (new MailboxAddress ("Aprajita Retails", "aprajitaretailsdumka@gmail.com"));
+            message.To.Add (new MailboxAddress (/*"",*/"amitnarayansah@gmail.com"));
+            if ( subjects != null )
+                message.Subject = "AprajitaRetails " + subjects + " ";
+            else
+                message.Subject = "AprajitaRetails Log:";
+            message.Body = new TextPart ("plain") { Text = messages };
+
+            using var client = new SmtpClient
+            {
+                // For demo-purposes, accept all SSL certificates (in case the server supports STARTTLS)
+                ServerCertificateValidationCallback = (s, c, h, e) => true
+            };
+            client.Connect ("smtp.gmail.com", 25, false);
+            // Note: only needed if the SMTP server requires authentication
+            client.Authenticate ("aprajitaretailsdumka@gmail.com", "ferrari265");
+            client.Send (message);
+            client.Disconnect (true);
+
+        }
+        public static void MailError(string messages)
+        {
+            MailLog (messages, "Error Log:");
+        }
+        public static void MailInfo(string messages) { MailLog (messages, "Info Log:"); }
+        public static void MailWarning(string messages) { MailLog (messages, "Warning Log:"); }
+
     }
 }
 
