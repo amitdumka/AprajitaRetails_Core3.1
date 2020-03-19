1c45efd8333ac53c48b4d15e2b1109ba349c5e3e
Amit Kumar <amitnarayansah@gmail.com>
2020-03-20 02:16:50 +0530

    bugs fixed

---
+++ AprajitaRetails/Areas/Sales/Controllers/ManualInvoiceController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AprajitaRetails.Areas.Voyager.Data;
+using AprajitaRetails.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AprajitaRetails.Areas.Sales.Controllers
+{
+    [Area ("Sales")]
+    public class ManualInvoiceController : Controller
+    {
+        private AprajitaRetailsContext aprajitaContext;
+        private VoyagerContext voyagerContext;
+
+        public ManualInvoiceController(AprajitaRetailsContext aCtx, VoyagerContext vCtx)
+        {
+            aprajitaContext = aCtx;
+            voyagerContext = vCtx;
+        }
+        public IActionResult Index()
+        {
+
+            return View ();
+        }
+
+        public IActionResult MainView()
+        {
+
+            return View ();
+        }
+        public JsonResult GetBarCode(string barcode)
+        {
+            try
+            {
+                var pItem = voyagerContext.ProductItems.Where (c => c.Barcode == barcode).Select (c => new { c.MRP, c.ProductName, c.TaxRate }).FirstOrDefault ();
+
+                if ( pItem == null )
+                {
+                    pItem = new { MRP = (decimal) 0.0, ProductName = "Not Found", TaxRate = (decimal) 0 };
+                }
+                JsonResult result = new JsonResult (pItem);
+                result.Value = pItem;
+                return result;
+
+            }
+            catch ( Exception )
+            {
+
+                var pItem = new { MRP = (decimal) 0.0, ProductName = "Not Found", TaxRate = (decimal) 0 };
+                JsonResult result = new JsonResult (pItem);
+                result.Value = pItem;
+                return result;
+
+            }
+
+
+
+            //Json { Data=pItem, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+        //public ActionResult SaveOrder(string name, String address, Order[] order)
+        //{
+        //    string result = "Error! Order Is Not Complete!";
+        //    if (name != null && address != null && order != null)
+        //    {
+        //        var cutomerId = Guid.NewGuid();
+        //        Customer model = new Customer();
+        //        model.CustomerId = cutomerId;
+        //        model.Name = name;
+        //        model.Address = address;
+        //        model.OrderDate = DateTime.Now;
+        //        db.Customers.Add(model);
+
+        //        foreach (var item in order)
+        //        {
+        //            var orderId = Guid.NewGuid();
+        //            Order O = new Order();
+        //            O.OrderId = orderId;
+        //            O.ProductName = item.ProductName;
+        //            O.Quantity = item.Quantity;
+        //            O.Price = item.Price;
+        //            O.Amount = item.Amount;
+        //            O.CustomerId = cutomerId;
+        //            db.Orders.Add(O);
+        //        }
+        //        db.SaveChanges();
+        //        result = "Success! Order Is Complete!";
+        //    }
+        //    return Json(result, JsonRequestBehavior.AllowGet);
+        //}
+    }
+}
--- AprajitaRetails/Areas/Sales/Models/SaleInvoice.cs
+++ AprajitaRetails/Areas/Sales/Models/SaleInvoice.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;    using System;
+﻿using AprajitaRetails.Areas.Purchase.Models;
+using Microsoft.AspNetCore.Authorization;    using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -35,5 +37,96 @@ namespace AprajitaRetails.Areas.Sales.Models
         public virtual ICollection<SaleItem> SaleItems { get; set; }
     }
 
+    public class ManualInvoice
+    {
+        public int ManualInvoiceId { get; set; } //Pk
+        [Display (Name = "Customer Name")]
+        public int CustomerId { get; set; }
+        [DataType (DataType.Date), DisplayFormat (DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true), Display (Name = "Sale Date")]
+        public DateTime OnDate { get; set; }
+        public string InvoiceNo { get; set; }
+        [Display (Name = "Total Items")]
+        public int TotalItems { get; set; }
+        [Display (Name = "Qty")]
+        public double TotalQty { get; set; }
+        [DataType (DataType.Currency), Column (TypeName = "money"), Display (Name = "Bill Amt")]
+        public decimal TotalBillAmount { get; set; }
+        [DataType (DataType.Currency), Column (TypeName = "money"), Display (Name = "Discount")]
+        public decimal TotalDiscountAmount { get; set; }
+        [DataType (DataType.Currency), Column (TypeName = "money"), Display (Name = "Round off")]
+        public decimal RoundOffAmount { get; set; }
+        [DataType (DataType.Currency), Column (TypeName = "money"), Display (Name = "Taxes")]
+        public decimal TotalTaxAmount { get; set; }
+        public virtual ManualSalePaymentDetail PaymentDetail { get; set; }
+        public virtual ICollection<ManualSaleItem> SaleItems { get; set; }
+    }
+    public class ManualCardPaymentDetail
+    {
+        [ForeignKey ("ManualSalePaymentDetail")]
+        public int ManualCardPaymentDetailId { get; set; }
+        public int ManualSaleInvoiceId { get; set; } // FK of SalesInvoice
+        public int CardType { get; set; }
+        [DataType (DataType.Currency), Column (TypeName = "money")]
+        public decimal Amount { get; set; }
+        public int AuthCode { get; set; }
+        public int LastDigit { get; set; }
+        [ForeignKey ("ManualSalePaymentDetailId")]
+        public virtual ManualSalePaymentDetail SalePaymentDetail { get; set; }
+    }
+    public class ManualSaleItem
+    {
+        public int ManualSaleItemId { get; set; }
 
+        public int ManualInvoiceId { get; set; }
+
+        public int ProductItemId { get; set; }
+        public virtual ProductItem ProductItem { get; set; }
+        public string BarCode { get; set; }
+
+        public double Qty { get; set; }
+        public Units Units { get; set; }
+
+        [DataType (DataType.Currency), Column (TypeName = "money")]
+        public decimal MRP { get; set; }
+
+        [DataType (DataType.Currency), Column (TypeName = "money")]
+        public decimal BasicAmount { get; set; }
+
+        [DataType (DataType.Currency), Column (TypeName = "money")]
+        public decimal Discount { get; set; }
+
+        [DataType (DataType.Currency), Column (TypeName = "money")]
+        public decimal TaxAmount { get; set; }
+
+        public int? SaleTaxTypeId { get; set; }
+        public virtual SaleTaxType SaleTaxType { get; set; }
+
+        [DataType (DataType.Currency), Column (TypeName = "money")]
+        public decimal BillAmount { get; set; }
+        // SaleTax options and it will Done
+
+        public int SalesPersonId { get; set; }
+
+        public virtual ManualInvoice SaleInvoice { get; set; }
+        public virtual SalesPerson Salesman { get; set; }
+    }
+    public class ManualSalePaymentDetail
+    {
+        [ForeignKey ("ManualInvoice")]
+        public int ManualSalePaymentDetailId { get; set; }
+        //public int SaleInvoiceId { get; set; }
+        public SalePayMode PayMode { get; set; }
+        [DefaultValue (0)]
+        [DataType (DataType.Currency), Column (TypeName = "money")]
+        public decimal CashAmount { get; set; }
+        [DefaultValue (0)]
+        [DataType (DataType.Currency), Column (TypeName = "money")]
+        public decimal CardAmount { get; set; }
+        [DefaultValue (0)]
+        [DataType (DataType.Currency), Column (TypeName = "money")]
+        public decimal MixAmount { get; set; }
+        public virtual ManualCardPaymentDetail CardDetails { get; set; }
+        public virtual ManualInvoice ManualInvoice { get; set; }
+    }
+    
 }
