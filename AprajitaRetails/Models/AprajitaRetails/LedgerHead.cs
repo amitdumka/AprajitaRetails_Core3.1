eed26774a77f97ffe6d8113987cd20c10ffc62f1
Amit Kumar <amitnarayansah@gmail.com>
2020-08-24 12:29:55 +0530

    basic addtion

---
+++ AprajitaRetails/Models/AprajitaRetails/LedgerHead.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AprajitaRetails.Models.AprajitaRetails
+{
+    public class LedgerHead
+    {
+        public int LedgerHeadId { get; set; }
+        public string HeadName { get; set; }
+        public bool IsExpense { get; set; }
+        public int? ParentId { get; set; }
+        public string Description { get; set; }
+    }
+}
+
+
+// Head Name: 
+
+/*
+ * Rent
+ * worshop rent
+ * workshop eletric bill
+ * Eletric bill
+ * Salary
+ * Incentive
+ * 
+ * 
+ */
+
